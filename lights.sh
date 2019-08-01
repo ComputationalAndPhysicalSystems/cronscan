837d6f3325ddcaee3b862a016ed1d5badb62e7bb
dustyyeti <yeti.shyn@gmail.com>
2019-08-01 14:43:46 -0700

    lightscript mods

--- lights.sh
+++ lights.sh
@@ -8,6 +8,25 @@
 #.	$1 >  on/off  { lower case req }
 
 OPTION=$1
+EP=$2
+
+
+while IFS= read -r line; do
+	echo line...
+    if [[ $line =~ debug ]]; then
+        echo "$line" >>./exp/t3/debug.txt
+    else
+        echo "$line" >>./exp/t3/info.txt
+    fi
+done <./exp/t3/t3.lights
+
+
+source $2.lights #: in one commad, loads all variables
+echo and... $L0
+for i in $(cat $2.lights); do
+	echo $i
+done
+
 
 #. hard coded
 
@@ -46,11 +65,13 @@ echo "turning lights $1"
 
 if [ "$OPTION" == "on" ]; then
 	for i in ${!LED[@]}; do
-		echo "<+$i*${LED[$i]}>" > $DEVICE
+		# echo "<+$i*${LED[$i]}>" > $DEVICE
+		echo "<+$i*${LED[$i]}>"
 	done
 else
 	for i in ${!LED[@]}; do
-		echo "<+$i*$OFF>" > $DEVICE
+		# echo "<+$i*$OFF>" > $DEVICE
+		echo "<+$i*$OFF>"
 	done
 
 fi
