5f8a8927f5a3e6cdb64f74ea777fe3650f9d42bf
zeth <dubo0720@vandals.uidaho.edu>
2019-09-17 18:03:13 -0700

    rsync test

--- transfer.sh
+++ transfer.sh
@@ -1,19 +1,29 @@
 #/bin/bash
+#command as seen in crontab: $sp/transfer.sh $ep
 
 LOCAL_DIR=$1
 EXPERIMENT_BASENAME=${LOCAL_DIR##*/}
 
-if [ ! -d "/run/user/1001/gvfs/smb-share:server=mnemosyne,share=experiments/$EXPERIMENT_BASENAME" ]; then
-    mkdir -p /run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$( hostname )/scannerData/$EXPERIMENT_BASENAME
-fi
+#! former samba code
+# if [ ! -d "/run/user/1001/gvfs/smb-share:server=mnemosyne,share=experiments/$EXPERIMENT_BASENAME" ]; then
+#     mkdir -p /run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$( hostname )/scannerData/$EXPERIMENT_BASENAME
+# fi
 
-for f in $LOCAL_DIR/*.tiff; do
-    cp "$f" /run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$(hostname)/scannerData/$EXPERIMENT_BASENAME/
+# for f in $LOCAL_DIR/*.tiff; do
+#     cp "$f" /run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$(hostname)/scannerData/$EXPERIMENT_BASENAME/
 	
-    if [ -f "/run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$(hostname)/scannerData/$EXPERIMENT_BASENAME/${f##*/}" ] ; then
-      rm $f
-      echo "File was copied successfully, removing local copy!"
-    else
-      echo "Mnemosyne not reached, local file not removed!"
-    fi
-done
+#     if [ -f "/run/user/1001/gvfs/smb-share:server=mnemosyne,share=data/CAPS/$(hostname)/scannerData/$EXPERIMENT_BASENAME/${f##*/}" ] ; then
+#       rm $f
+#       echo "File was copied successfully, removing local copy!"
+#     else
+#       echo "Mnemosyne not reached, local file not removed!"
+#     fi
+# done
+
+#! test if "exp" is a directory
+# test -d exp && echo yes || echo no
+#caps@129.101.130.89:/beta/data/CAPS/experiment/
+
+#rsync will create directories while creating files if necessary
+echo "Moving image files to Mnemosyne" 
+rsync -vha --progress --remove-source-files "$1/*.png" "caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME"
