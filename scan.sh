72ba828515d4d174fb8dfd0cab41299f0411f203
zeth <dubo0720@vandals.uidaho.edu>
2019-09-18 17:23:56 -0700

    .

--- scan.sh
+++ scan.sh
@@ -57,7 +57,7 @@ done
 
 export APP_SLACK_WEBHOOK=https://hooks.slack.com/services/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 #slime-report channel
 
-test -e $2/count && slack "[UPDATE} SCAN# $ENUM" || slack "[UPDATE] First scan for experiment $EXPERIMENT_BASENAME"
+test -e $2/count && slack "[UPDATE] SCAN# $ENUM" || slack "[UPDATE] First scan for experiment $EXPERIMENT_BASENAME"
 echo $ENUM > $LOCAL_DIR/count
 rsync -ha --progress $2/LOG caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME
 rsync -ha --progress $2/count caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME
