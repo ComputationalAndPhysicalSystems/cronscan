e15318b380996e43c1b55754f10a1d5126e3dca2
Zeth <zdubois@uidaho.edu>
2020-01-23 18:30:22 -0800

    .

--- blue.py
+++ blue.py
@@ -8,16 +8,16 @@ DMA = 5
 INVERT = False # Invert required when using inverting buffer
 BRIGHTNESS = 255
 
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
 
 strip = Adafruit_NeoPixel(LEDCOUNT, GPIOPIN, FREQ, DMA, INVERT, BRIGHTNESS)
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
-colorWipe(strip, Color(0,0,0), 10)
+strip.setPixelColor(0, Color(0,0,255))
+strip.setPixelColor(1, Color(0,0,255))
+strip.setPixelColor(2, Color(0,0,255))
+strip.setPixelColor(3, Color(0,0,255))
+strip.setPixelColor(4, Color(0,0,255))
+strip.setPixelColor(5, Color(0,0,255))								
+strip.show()
+
