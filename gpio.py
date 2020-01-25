df80be28631e43d69b3e8861167cb72f7b880dd0
zeth <dubo0720@vandals.uidaho.edu>
2020-01-24 16:22:32 -0800

    gpiooooooooooo

--- gpio.py
+++ gpio.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-#from neopixel import *
-#import time
+from neopixel import *
+import time
 
 with open("/home/caps/scripts/caps_cronscan/exp/current.env") as infile:
     Exp, Cnt = map(str, infile.read().split())
-
-#print(Exp)
-#print(Cnt)
+Msg = "/home/caps/scripts/caps_cronscan/exp/" + Exp +"/py.pylog"
 
 LEDCOUNT = Cnt # Number of LEDs
 GPIOPIN = 21
@@ -15,23 +13,16 @@ DMA = 5
 INVERT = False # Invert required when using inverting buffer
 BRIGHTNESS = 255
 
+strip = Adafruit_NeoPixel(LEDCOUNT, GPIOPIN, FREQ, DMA, INVERT, BRIGHTNESS)
 
-#strip = Adafruit_NeoPixel(LEDCOUNT, GPIOPIN, FREQ, DMA, INVERT, BRIGHTNESS)
 # Intialize the library (must be called once before other functions).
-#strip.begin()
+strip.begin()
 
-
-
-#with open("/home/caps/scripts/caps_cronscan/exp/current.env") as infile:
- #   k = map(str, infile.read())
-
-Msg = "/home/caps/scripts/caps_cronscan/exp/" + Exp +"/py.pylog"
-#print(Msg)
+# get the last values to set the neopixels
 f1 = open(Msg, "r")
 Last_line = f1.readlines()[-1]
 f1.close()
 
-#print(Last_line)
 
 def split(line):
 	return [char for char in line]
