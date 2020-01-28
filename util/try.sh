a35e429c722f1c12876523610d23a3968b3acc66
Zeth <zdubois@uidaho.edu>
2020-01-27 16:37:39 -0800

    .

--- util/try.sh
+++ util/try.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # https://www.youtube.com/watch?v=jeq161yD8tk
-
+options=($(find ../exp -maxdepth 2 -iname '*.exp' -print0|xargs -0 )) 
 
 echo "select file"
-options=($(find /home/caps/scripts/caps_cronscan/exp -maxdepth 2 -iname '*.exp' -print0|xargs -0))
-
 select opt in "${options[@]}" "Quit";
 do
-	if (( REPLY == 1 + ${#options[@]}))
+	if ((REPLY == 1 + ${#options[@]}))
 	then
 		exit
-	elif (( REPLY > 0 && REPLY <= ${options[@]}))
+	elif (( REPLY <= ${#options[@]}))
 	then
-		echo "File selected $opt"
+		display=$(basename $opt)
+		echo $opt
+		echo "File selected $display"
 		break
 	else
 		echo "Not valid"
