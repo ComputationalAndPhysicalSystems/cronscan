2fb07843410dbc65b6043c60df39b420a13c0514
conrad <mear8979@vandals.uidaho.edu>
2020-03-09 22:19:59 -0700

    .

--- util/scan.sh
+++ util/scan.sh
@@ -9,53 +9,54 @@ source /usr/local/bin/caps_settings/physarumhook
 source /usr/local/bin/caps_settings/slimehook
 
 RESOLUTION=$1
-EP=$2
+LOCAL_DIR=$2
 #DELAY=4
 
-SCANS=$(($(cat $EP/count)+1))
-EXPERIMENT_BASENAME=${EP##*/}
+ENUM=$(($(cat $LOCAL_DIR/count)+1))
+EXPERIMENT_BASENAME=${LOCAL_DIR##*/}
 
 export SANE_USB_WORKAROUND=1
 
-[[ $LIGHTS == "on" ]] && $LABPATH/util/lights.sh off $EP 2>&1 | tee -a $EP/LOG #. turn of lights if exp is using
-
-echo "==Beginning Scan=="
-date
-echo "Scan count: $SCANS"
-echo "Experiment \"$EXPERIMENT_BASENAME\" will be stored in $EP"
+now=$(date)
+nows=$(date +%s)
+echo "==Beginning Scan================================="
+echo $now
+echo $nows
+echo "Scan count: $ENUM"
+echo "Experiment \"$EXPERIMENT_BASENAME\" will be stored in $LOCAL_DIR"
 
 # Create experiment direcotry if it doesn't already exist
-if [ ! -d "$EP" ]; then
-    echo "$EP not found, creating..."
-    mkdir -p $EP
+if [ ! -d "$LOCAL_DIR" ]; then
+    echo "$LOCAL_DIR not found, creating..."
+    mkdir -p $LOCAL_DIR
 fi
 
 SCANNER_LIST=$(scanimage -f "%d%n")
 SCANNER_COUNT=$(echo "$SCANNER_LIST" | wc -l)
 
 # Have we stored information about scanner count?
-if [ ! -f "$EP/scanners" ]; then
-    echo "$SCANNER_COUNT" > "$EP/scanners"
+if [ ! -f "$LOCAL_DIR/scanners" ]; then
+    echo "$SCANNER_COUNT" > "$LOCAL_DIR/scanners"
 fi
 
-echo "Found $SCANNER_COUNT/$(cat $EP/scanners) scanners:"
+echo "Found $SCANNER_COUNT/$(cat $LOCAL_DIR/scanners) scanners:"
 echo "$SCANNER_LIST"
 
-if [ "$SCANNER_COUNT" -lt "$(cat $EP/scanners)" ]
+if [ "$SCANNER_COUNT" -lt "$(cat $LOCAL_DIR/scanners)" ]
 then
-	slack "[LAB ALERT] <EXP: $EXPERIMENT_BASENAME>: Only detected $SCANNER_COUNT/$(cat $EP/scanners) scanners. Scanners may require physical inspection."
+	slack "[LAB ALERT] <EXP: $EXPERIMENT_BASENAME>: Only detected $SCANNER_COUNT/$(cat $LOCAL_DIR/scanners) scanners. Scanners may require physical inspection."
 	source /usr/local/bin/caps_settings/slimehook
-    slack "[WARNING]: Only detected $SCANNER_COUNT/$(cat $EP/scanners) scanners."
-    slack "RIP Acquisition #$SCANS, ~$(date +%s)"
+    slack "[WARNING]: Only detected $SCANNER_COUNT/$(cat $LOCAL_DIR/scanners) scanners."
+    slack "RIP Acquisition #$ENUM, ~$(date +%s)"
 fi
 si=1
 for scanner in $SCANNER_LIST; do
     scanner_safename=${scanner//:/_}
-    FILENAME="$SCANS.$EXPERIMENT_BASENAME.s$si.$(date +%s).png" #$scanner_safename.$(date +%s).png
+    FILENAME="$ENUM.$EXPERIMENT_BASENAME.s$si.$nows.png"
 
     echo "Scanning $scanner to $FILENAME"
 
-    scanimage -d $scanner --mode Color --format png --resolution $RESOLUTION > $EP/$FILENAME
+    scanimage -d $scanner --mode Color --format png --resolution $RESOLUTION > $LOCAL_DIR/$FILENAME
     ((si++))
 #	echo "Delaying for $DELAY seconds"
 #	sleep $DELAY
@@ -67,21 +68,15 @@ test -e $2/count && echo || slack "[LAUNCH] First scan for experiment $EXPERIMEN
 
 source /usr/local/bin/caps_settings/slimehook
 
-if [ $(( $SCANS % $SLACK_INTERVAL )) -eq 0 ]
+if [ $(( $ENUM % $SLACK_INTERVAL )) -eq 0 ]
 then
-    slack "[UPDATE] SCAN# $SCANS"
+    slack "[UPDATE] SCAN# $ENUM"
 fi
 
-echo $SCANS > $EP/count
-echo EXP=$EXP > $LABPATH/exp/status.env
-echo DISH_CNT=$DISH_CNT >> $LABPATH/exp/status.env
-echo SCANNERS=$SCANNERS >> $LABPATH/exp/status.env
-echo SCANS=$SCANS >> $LABPATH/exp/status.env
-echo STATUS=running >> $LABPATH/exp/status.env
-
-rsync $2/LOG caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
+echo $ENUM > $LOCAL_DIR/count
+rsync $2/*.exp caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
 rsync $2/count caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
 rsync $2/xtab caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
-rsync $2/*.exp caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
-rsync $2/*.lights caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
+rsync $2/*.pylog caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
 rsync $2/*.log caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
+rsync $2/LOG caps@129.101.130.89:/beta/data/CAPS/experiments/$EXPERIMENT_BASENAME/
