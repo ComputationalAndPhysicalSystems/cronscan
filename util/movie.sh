3e7fc1a1c98613ef8ed7d0202b8bdb28d4872c9c
zeth <dubo0720@vandals.uidaho.edu>
2020-04-01 16:34:12 -0700

    .

--- util/movie.sh
+++ util/movie.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# $1=exp name $2=scanner counter
+# $1=exp name
+EXPFILE=$LABPATH/exp/$1/$1.exp
+echo "sourcing job file: $EXPFILE"
+source $EXPFILE
+#source /usr/local/bin/cronscan/config
+
 r="20"
 f="image2"
 s="1700x2354"
@@ -7,14 +12,17 @@ vcodec="libx264"
 crf="25"
 pix_fmt="yuv420p"
 dir="~/lab"
+now=$(date)
 
 
-for scanner in $2; do
+for scanner in $SCANNERS; do
   i2="${dir}/${1}/${i_pre}.${1}.s${scanner}.png"
   path="${dir}/${1}/"
   suf=".${1}.s${scanner}.png"
   movie="${1}.s${scanner}.mp4"
-  ffmpeg -r $r -f $f -s $s -i $1/%04d.$1.s$2.png -vcodec $vcodec -crf $crf -pix_fmt $pix_fmt $movie
+  ffmpeg -r $r -f $f -s $s -i $1/%04d.$1.s$SCANNERS.png -vcodec $vcodec -crf $crf -pix_fmt $pix_fmt $movie
 
 done
+
+echo "$EXP movie genrated $now. R $RELEASE" >> joblog
 
