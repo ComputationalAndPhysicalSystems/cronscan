d141ee07bd4e63706fe6d33bfd46a291149f7119
zeth <dubo0720@vandals.uidaho.edu>
2020-04-01 17:04:36 -0700

    .

--- util/movie.sh
+++ util/movie.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-# $1=exp name
-EXPFILE=$LABPATH/exp/$1/$1.exp
-echo "sourcing job file: $EXPFILE"
-source $EXPFILE
-#source /usr/local/bin/cronscan/config
+# $1=exp name $2=scanner count
+
+#??? inteneded use:
+# ssh -A caps@129.101.130.90 'cd ~/lab/movie; bash -s' < movie.sh test
+#?- doesn't work: can't source this info during a ssh call to remote server, apparently
+#?- wanted to source the exp record to get the EXP variable (although redundant) and the SCANNERCOUNT
+
+#?---------- makes the following procedure useless:
+#x EXPFILE=$LABPATH/exp/$1/$1.exp
+#x echo "sourcing job file: $EXPFILE"
+#x source $EXPFILE
+#----------------------
+
+#? workaround: source info before starting and pass parameters to the script manually
+#? add $2=scanner count
+
+#-------------
+
 
 r="20"
 f="image2"
@@ -15,12 +28,12 @@ dir="~/lab"
 now=$(date)
 
 
-for scanner in $SCANNERS; do
+for scanner in $2; do
   i2="${dir}/${1}/${i_pre}.${1}.s${scanner}.png"
   path="${dir}/${1}/"
   suf=".${1}.s${scanner}.png"
   movie="${1}.s${scanner}.mp4"
-  ffmpeg -r $r -f $f -s $s -i $1/%04d.$1.s$SCANNERS.png -vcodec $vcodec -crf $crf -pix_fmt $pix_fmt $movie
+  ffmpeg -r $r -f $f -s $s -i $1/%04d.$1.s$2.png -vcodec $vcodec -crf $crf -pix_fmt $pix_fmt $movie
 
 done
 
