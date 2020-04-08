4e5ace0a254ce5731fc1505e30a701908db542c9
zeth <zdubois@uidaho.edu>
2020-04-07 21:36:18 -0700

    .progress.

--- util/crop.sh
+++ util/crop.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 # test image offset
-# $1: offset-x, $2: offset-y
+# reqd $1: exp name $2: path
+# optional $3: offset-x, $4: offset-y
 
 #.. SOURCES
 #.  source golbal
@@ -21,69 +22,79 @@ echo "<<crop.sh>> | name=$1 | path=$2 | offsetx=$3 | offsety=$4"
 printf '~%.0s' {1..29}
 echo
 
-#.  local vars
-S=696x696 #: image dims
-O1=900+79
-O2=900+868
-O3=900+1656
-O4=116+1656
-O5=116+868
-O6=116+79
+#.. local vars
+#. dynamic vars
+label=()
+crops=()
+
+#.  constants
+
+crops+=("696x696") #. crop[0] = image size
+crops+=("900+79")
+crops+=("900+868")
+crops+=("900+1656")
+crops+=("116+1656")
+crops+=("116+868")
+crops+=("116+79")
+
+S=${crops[0]}
+
+label+=("dummy")
 
 for i in {1..6}
 do
-  L${i}=$i
-  echo $i
-  echo $L${i}
+  label+=("$1: plate $i")
 done
-echo here is L1 $L1
-
-L1="${1}: plate 1"
-L2="${1}: plate 2"
-L3="${1}: plate 3"
-L4="${1}: plate 4"
-L5="${1}: plate 5"
-L6="${1}: plate 6"
-
-[ -d $2/1 ] || mkdir $2/1
-[ -d $2/2 ] || mkdir $2/2
-[ -d $2/3 ] || mkdir $2/3
-[ -d $2/4 ] || mkdir $2/4
-[ -d $2/5 ] || mkdir $2/5
-[ -d $2/6 ] || mkdir $2/6
 
 
+for i in {1..6}
+do
+  [ -d $2/$i ] || mkdir $2/$i
+done
 
-convert $2/${1}.png -crop $S+$O1 $2/1/${1}_1.png
-convert $2/${1}.png -crop $S+$O2 $2/2/${1}_2.png
-convert $2/${1}.png -crop $S+$O3 $2/3/${1}_3.png
-convert $2/${1}.png -crop $S+$O4 $2/4/${1}_4.png
-convert $2/${1}.png -crop $S+$O5 $2/5/${1}_5.png
-convert $2/${1}.png -crop $S+$O6 $2/6/${1}_6.png
+#. first crop
+# for i in {1..6}
+# do
+#   convert $2/${1}.png -crop $S+${crops[$i]} $2/${i}/${1}_${i}.png
+# done
 
-convert $2/1/${1}_1.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/1/${1}_1.png
-convert $2/2/${1}_2.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/2/${1}_2.png
-convert $2/3/${1}_3.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/3/${1}_3.png
-convert $2/4/${1}_4.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/4/${1}_4.png
-convert $2/5/${1}_5.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/5/${1}_5.png
-convert $2/6/${1}_6.png -background black -splice 0x20 -gravity south \
--splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
-"$L1" $2/6/${1}_6.png
+#. apply labels
+# for i in {1..6}
+# do
+#   echo $i
+#   echo ${label[$i]}
+#   # read
+#   convert $2/${i}/${1}_${i}.png -background black -splice 0x20 -gravity south \
+#   -splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
+#   "${label[$i]}" $2/${i}/${1}_${i}x.png
+# done
 
 
-convert $2/1/${1}_1.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/1/${1}_1.png
-convert $2/2/${1}_2.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/2/${1}_2.png
-convert $2/3/${1}_3.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/3/${1}_3.png
-convert $2/4/${1}_4.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/4/${1}_4.png
-convert $2/5/${1}_5.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/5/${1}_5.png
-convert $2/6/${1}_6.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/6/${1}_6.png
+# convert $2/2/${1}_2.png -background black -splice 0x20 -gravity south \
+# -splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
+# "$L1" $2/2/${1}_2.png
+# convert $2/3/${1}_3.png -background black -splice 0x20 -gravity south \
+# -splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
+# "$L1" $2/3/${1}_3.png
+# convert $2/4/${1}_4.png -background black -splice 0x20 -gravity south \
+# -splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
+# "$L1" $2/4/${1}_4.png
+# convert $2/5/${1}_5.png -background black -splice 0x20 -gravity south \
+# -splice 0x20 -pointsize 18 -fill white -gravity north -annotate +150+84 \
+# "$L1" $2/5/${1}_5.png
+# i=5
+# l=${label[5]}
+# y="boooo"
+for i in {1..6}
+do
+convert $2/${i}/${1}_${i}x.png -background black -fill white label:"${label[$i]}" \
+        +swap -gravity west -append $2/${i}/${1}_${i}.png
+done
+#
+#
+# convert $2/1/${1}_1.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/1/${1}_1.png
+# convert $2/2/${1}_2.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/2/${1}_2.png
+# convert $2/3/${1}_3.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/3/${1}_3.png
+# convert $2/4/${1}_4.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/4/${1}_4.png
+# convert $2/5/${1}_5.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/5/${1}_5.png
+# convert $2/6/${1}_6.png -fill white -draw "rectangle 350,0 696,20 rectangle 350,716 696,736" $2/6/${1}_6.png
--- util/frags.sh
+++ util/frags.sh
@@ -6,7 +6,6 @@ for i in {1..6}
 do
   resultarray+=($B)
   label+=("name, plate $i")
-
 done
 echo ${label[@]}
 #-------------
