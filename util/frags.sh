6a27e59cefb4e0ed4c0bf995e90ace63008239c4
zeth <zdubois@uidaho.edu>
2020-04-15 15:30:56 -0700

    report automation

--- util/frags.sh
+++ util/frags.sh
@@ -50,8 +50,12 @@
 # echo ${!varname}
 
 #------------.
-source $LABPATH/.func/assigned
-scanner=1
-cropfile=$TRACKDIR/s${scanner}.crop
-SCANFILE=0001.theoretical.s1.45464564.png
-rsync $cropfile caps@129.101.130.90:~/lab/movie/$SCANFILE.crop
+# source $LABPATH/.func/assigned
+# scanner=1
+# cropfile=$TRACKDIR/s${scanner}.crop
+# SCANFILE=0001.theoretical.s1.45464564.png
+# rsync $cropfile caps@129.101.130.90:~/lab/movie/$SCANFILE.crop
+for f in *png; do
+  mv "$f" "${f%??????????????}png"
+   #mv -- "$f" "${f:0:$position-1}${f:$position}"
+done
